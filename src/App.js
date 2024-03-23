2d62638247376a37214b542f9e20149f057dde71
AakashSingh20 <akashsingh20603@gmail.com>
2024-03-23 17:01:42 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [submitted, setsubmitted] = useState(false);
+
+  const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const submit = () => {
+    setsubmitted(true);
+  };
+
+  const close = () => {
+    setsubmitted(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="layout">
+        <div className="header"></div>
+        <div className="middle">
+          <div className="left">
+            <div className="body">
+              <div className="title">title</div>
+              <div className="dock">a b c d</div>
+              <div className="textarea">
+                <textarea
+                  className="custom"
+                  placeholder="Enter text here"
+                ></textarea>
+              </div>
+            </div>
+
+            <div className="footer">
+              {windowWidth < 1024 && submitted && data.length > 0 ? (
+                <div className="p">
+                  <button onClick={close}>close</button>
+                  <div className="card">card</div>
+                </div>
+              ) : (
+                <div className="save_row">
+                  <button className="button">save</button>
+                  <button className="button" onClick={submit}>
+                    submit
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
+          {submitted && data.length > 0 ? (
+            <div className="right">
+              <button onClick={close}>close</button>
+              <div className="card">card</div>
+            </div>
+          ) : null}
+        </div>
+      </div>
+    </>
   );
 }
 
